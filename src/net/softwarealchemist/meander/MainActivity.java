1c4a0f86066db98551ee66a3ad3e3fd874cefe8e
andypaxo <andy@andypaxo.net>
2014-04-28 18:45:15 -0600

    Full screen for Android 4.4+

--- src/net/softwarealchemist/meander/MainActivity.java
+++ src/net/softwarealchemist/meander/MainActivity.java
@@ -1,10 +1,12 @@
 package net.softwarealchemist.meander;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
 import android.opengl.GLSurfaceView;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
-
 import com.google.android.gms.analytics.HitBuilders;
 import com.google.android.gms.analytics.Tracker;
 
@@ -36,5 +38,20 @@ public class MainActivity extends Activity {
 		Tracker tracker = ((MeanderApp)getApplication()).getTracker();
     	tracker.setScreenName("Main Activity");
     	tracker.send(new HitBuilders.AppViewBuilder().build());
+	}
+	
+	@TargetApi(Build.VERSION_CODES.KITKAT)
+	@Override
+	public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+	    if (hasFocus) {
+	        mGLView.setSystemUiVisibility(
+	                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+	                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+	                | View.SYSTEM_UI_FLAG_FULLSCREEN
+	                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+	        }
 	}
 }
